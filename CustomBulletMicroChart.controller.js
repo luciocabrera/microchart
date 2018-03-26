70ed8eaef1b24c91b22a68d928c503536f54ed10
luciocabrera <lucio.cabrera@just-bi.nl>
2018-03-26 12:02:31 +0200

    cleaning the code

--- CustomBulletMicroChart.controller.js
+++ CustomBulletMicroChart.controller.js
@@ -1,18 +1,14 @@
-sap.ui.define(
-  ['sap/m/MessageToast', 'sap/ui/core/mvc/Controller', 'microchart/formatter'],
-  function(MessageToast, Controller, formatter) {
-    'use strict';
+sap.ui.define(['sap/m/MessageToast', 'sap/ui/core/mvc/Controller'], function(
+  MessageToast,
+  Controller
+) {
+  'use strict';
 
-    var PageController = Controller.extend(
-      'microchart.CustomBulletMicroChart',
-      {
-        formatter: formatter,
-        press: function(oEvent) {
-          MessageToast.show('The bullet micro chart is pressed.');
-        }
-      }
-    );
+  var PageController = Controller.extend('microchart.CustomBulletMicroChart', {
+    press: function(oEvent) {
+      MessageToast.show('The bullet micro chart is pressed.');
+    }
+  });
 
-    return PageController;
-  }
-);
+  return PageController;
+});
